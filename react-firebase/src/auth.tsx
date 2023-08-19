90b0ef20960a2b098ffbaa5ffc2bd5d5db118f4e
Gabriel Pająk <Gabriel.Pajak@cshark.com>
2023-08-19 20:10:04 +0300

    Add react app.

--- react-firebase/src/auth.tsx
+++ react-firebase/src/auth.tsx
@@ -3,13 +3,7 @@ import { initializeApp } from 'firebase/app'
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCF2rdRdpPBtYRZxIoFVSVttG_JxBPt7ck",
-  authDomain: "reference-app-2820e.firebaseapp.com",
-  projectId: "reference-app-2820e",
-  storageBucket: "reference-app-2820e.appspot.com",
-  messagingSenderId: "914112482301",
-  appId: "1:914112482301:web:1fd8f75835c02d0e532794",
-  measurementId: "G-3HN9WCPQM9"
+
 };
 
 export const firebaseApp = initializeApp(firebaseConfig);
